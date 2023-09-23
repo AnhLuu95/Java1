ba108147a1aa3700cddc9a8e59b949ae645e51b1
AnhLuu <145408418+AnhLuu95@users.noreply.github.com>
2023-09-23 11:22:58 +0700

    Add files via upload

---
+++ SoNguyenTo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package So.Nguyen.To;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SoNguyenTo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        
+    }
+    
+}
