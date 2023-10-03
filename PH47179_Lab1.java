9645d074d6ac528319d5d77b4ac50d21e6b07dbe
AnhLuu <145408418+AnhLuu95@users.noreply.github.com>
2023-10-03 14:38:04 +0700

    Add files via upload

---
+++ PH47179_Lab1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ph47179_lab1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class PH47179_Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
